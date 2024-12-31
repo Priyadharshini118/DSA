665f8c81c01338e85ff9bf21bf6ffdb56c1120b0
priyadharshini118 <priyaraja1108ar@gmail.com>
2024-12-31 12:26:59 +0530

    v2

---
+++ Scaler_DSA/src/Array/Max_of_Array.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Max_of_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		int max=Integer.MIN_VALUE;
+		for(int i:arr) {
+			max=Math.max(max, i);
+		}
+		System.out.println(max);
+		sc.close();
+	}
+
+}
